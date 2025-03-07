c482257efb3077b20d225979dfe1a4bd3b4a89c6
kevinduhamelhayes <kevinduhamelh@gmail.com>
2025-03-07 01:33:11 -0300

    fix: correct ThemeProvider props in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider defaultTheme="system" storageKey="airbnb-theme">
           <div className="relative flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
